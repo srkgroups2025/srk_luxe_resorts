d04ef718e642b3a57b52a67cbac95029e1d874e0
srkgroups2025 <srkgroups2025@gmail.com>
2026-01-07 18:25:01 +0530

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -24,7 +24,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://srk-luxe-resorts.vercel.app",
-  "https://www.srkluxeresortsudumalpet.com/",
+  "https://www.srkluxeresortsudumalpet.com",
 ];
 
 app.use(
