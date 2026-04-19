ccefcc0f2fb6ff8794eaf0242b5d3f8a0c9a10cc
srkgroups2025 <srkgroups2025@gmail.com>
2026-04-19 13:25:50 +0530

    Update next.config.ts

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: ".",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
